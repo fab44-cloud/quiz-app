60d719010caded198d129ce69d202a4b4c0185ca
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-05-30 14:09:50 -0600

    Use props to display each question and answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,13 @@
 export default function Question(props) {
+    console.log("Props:", props)
+
     return (
         <div className="question-container">
-            <h3>This is a question?</h3>
+            <h3>{props.question}</h3>
             <div className="choices-container">
-                <button>choice</button>
-                <button>choice</button>
-                <button>choice</button>
-                <button>choice</button>
+                {props.answers.map((choice) => {
+                    return <button key={choice}>{choice}</button>
+                })}
             </div>
             <hr />
         </div>
