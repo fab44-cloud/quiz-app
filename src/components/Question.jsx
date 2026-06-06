15c46032a6b8817acb34763e6cca61c5701c9990
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-06-06 17:03:20 -0600

    style: add button class for styling correct and incorrect answers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,17 +1,23 @@
-export default function Question(props) {
+import clsx from 'clsx'
 
+export default function Question(props) {
     return (
         <div className="question-container">
             <h3>{props.question}</h3>
             <div className="choices-container">
                 {props.answers.map((choice) => {
-                    const buttonClass = choice.isSelected ? "choice-btn selected" : "choice-btn"
+                    const buttonClass = clsx("choice-btn", {
+                        selected: !props.quizOver && choice.isSelected,
+                        correct: props.quizOver && choice.isCorrect,
+                        incorrect: props.quizOver && (choice.isSelected && !choice.isCorrect),
+                        faded: props.quizOver && (!choice.isSelected && !choice.isCorrect)
+                    })
 
                     return (
                         <button
                             key={choice.id} 
                             className={buttonClass}
-                            onClick={() => props.handleSelect(props.id, choice.id)}
+                            onClick={() => !props.quizOver && props.handleSelect(props.id, choice.id)}
                             >
                                 {choice.text}
                         </button>
