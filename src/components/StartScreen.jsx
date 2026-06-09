77fc4e244ca1aba22d53cde82c190010a27d1abd
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-06-08 22:48:24 -0600

    Add container for styling

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,6 +1,6 @@
 export default function StartScreen(props) {
     return (
-        <div>
+        <div className="start-screen-container">
             <h1>Quizzical</h1>
             <p>Test your knowledge with 5 random trivia questions!</p>
             <button onClick={props.handleClick}>Start quiz</button>
