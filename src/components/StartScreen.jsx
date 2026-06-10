d390e19e02034c65e2c6fb9efdb96d8f0c7d8e3b
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-06-09 23:12:07 -0600

    Update class name

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,6 +1,6 @@
 export default function StartScreen(props) {
     return (
-        <div className="start-screen-container">
+        <div className="quiz-container">
             <h1>Quizzical</h1>
             <p>Test your knowledge with 5 random trivia questions!</p>
             <button onClick={props.handleClick}>Start quiz</button>
